7b1e676e4b37c29948963b46fc2d984e79ee3c76
dacolombo <daniele.colombo39@gmail.com>
2022-08-09 10:20:51 +0000

    Change dstat client into general ssh

--- dstat.py
+++ dstat.py
@@ -2,32 +2,32 @@ import subprocess
 import time
 
 
-class DstatClient:
+class SSHClient:
 
     def __init__(self, ssh_key, ssh_user, endpoint_ip):
         self.ssh_key = ssh_key
         self.ssh_user = ssh_user
         self.endpoint = endpoint_ip
 
-    def build_command(self, cmd):
-        return f'ssh -i {self.ssh_key} {self.ssh_user}@{self.endpoint} "{cmd}"'
+    def run_command(self, cmd, wait=True):
+        cmd = f'ssh -i {self.ssh_key} {self.ssh_user}@{self.endpoint} "{cmd}"'
+        if wait:
+            subprocess.Popen(cmd, shell=True).wait()
+        else:
+            subprocess.Popen(cmd, shell=True)
 
     def install_dstat(self):
-        cmd = self.build_command("sudo yum install -y dstat")
-        subprocess.Popen(cmd, shell=True).wait()
+        self.run_command("sudo yum install -y dstat")
 
     def prepare_dstat_dir(self, dstat_output_dir):
         self.output_dir = dstat_output_dir
-        cmd = self.build_command(f"rm -rf {self.output_dir}; mkdir -p {self.output_dir}")
-        subprocess.Popen(cmd, shell=True).wait()
+        self.run_command(f"rm -rf {self.output_dir}; mkdir -p {self.output_dir}")
 
     def run_dstat(self, device, dstat_output_file):
-        cmd = self.build_command(f"dstat --disk-tps -d -t --noheaders -o {self.output_dir}/{dstat_output_file} -D {device} > /dev/null")
-        subprocess.Popen(cmd, shell=True)
+        self.run_command(f"dstat --disk-tps -d -t --noheaders -o {self.output_dir}/{dstat_output_file} -D {device} > /dev/null", wait=False)
 
     def kill_dstat(self):
-        cmd = self.build_command("pkill -9 dstat > /dev/null")
-        subprocess.Popen(cmd, shell=True).wait()
+        self.run_command("pkill -9 dstat > /dev/null")
 
     def get_dstat_out(self, path):
         scp_cmd = f'scp -r -i {self.ssh_key} {self.ssh_user}@{self.endpoint}:{self.output_dir} {path}'
